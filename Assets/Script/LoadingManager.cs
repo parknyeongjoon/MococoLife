0b6c2db2569bc53104c90cf79bc7065be284a4a4
foba1 <foba1@naver.com>
2022-10-06 22:53:10 +0900

    Add LobbyManager & LoadingManager.
    Add LobbyManager & LoadingManager. Implement function to connect photon server and wait for another players with loading icon & text.

---
+++ Assets/Script/LoadingManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LoadingManager : MonoBehaviourPun
+{
+    private float timeCount;
+    private int playerCount;
+
+    public Text playerText;
+    public Text loadingText;
+    public GameObject loadingObject;
+
+    private void Start()
+    {
+        timeCount = 0f;
+        playerCount = 1;
+        playerText.text = "1 / 4";
+    }
+
+    private void Update()
+    {
+        if (PhotonNetwork.PlayerList.Length < 4)
+        {
+            if (timeCount <= 1.5f) timeCount += Time.deltaTime;
+            if (timeCount <= 0.5f)
+            {
+                loadingText.text = "플레이어를 기다리는중.";
+            }
+            else if (timeCount <= 1f)
+            {
+                loadingText.text = "플레이어를 기다리는중..";
+            }
+            else if (timeCount <= 1.5f)
+            {
+                loadingText.text = "플레이어를 기다리는중...";
+            }
+            else
+            {
+                timeCount = 0f;
+            }
+            loadingObject.transform.eulerAngles += new Vector3(0f, 0f, 0.1f);
+        }
+        else
+        {
+            PhotonNetwork.LoadLevel("Stage");
+        }
+
+        if (PhotonNetwork.PlayerList.Length != playerCount)
+        {
+            playerCount = PhotonNetwork.PlayerList.Length;
+            playerText.text = playerCount.ToString() + " / 4";
+        }
+    }
+}
---
+++ Assets/Script/LobbyManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private string gameVersion = "1";
+
+    public Text connectStateText;
+
+    private void Start()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            connectStateText.text = "Online";
+        }
+        else
+        {
+            connectStateText.text = "Connecting to server...";
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        connectStateText.text = "Online";
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        connectStateText.text = "Fail to connect server...";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void QuickMatching()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            connectStateText.text = "Quick matching...";
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            connectStateText.text = "Fail to connect server...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Loading");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom("Room_" + Random.Range(0, 1000), new RoomOptions { MaxPlayers = 4 });
+    }
+}
