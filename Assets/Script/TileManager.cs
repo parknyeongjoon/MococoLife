e2b448bce0862fee0a20a1245ef21f183ad9e6d6
parknyeongjoon <idpasudwns@naver.com>
2022-10-06 14:11:20 +0900

    ㅁㅇㅁ
    ㅁㅇㅁ

--- Assets/Script/TileManager.cs
+++ Assets/Script/TileManager.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    List<GameObject> BaltanAreas;
+
     void Start()
     {
-        
+        for(int i = 0; i < 10; i++)
+        {
+            int rand = Random.Range(0, BaltanAreas.Count);
+            Instantiate(BaltanAreas[rand], new Vector3(30 * i, 0, 0), Quaternion.identity);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
